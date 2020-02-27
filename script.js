9e16eda781f668ec67650d279cd8b87a16c5547c
Kryshtene Tesfaye <kryshtenet@outlook.com>
2020-02-27 04:43:19 -0500

    Updated the form in java scriptl

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-//Event Listener for our search city button
-$("#city-button").on("click",function(){
-  
-}
+    //Event listener for the city-search button
+    $("#citySearchBtn").on("click", function() {
+        //Grabbing and storing the data-cities property value from the button
+        var pastSearches = $(this).attr("data-cities")
 
+    // Storing the Open Weather Map API URL for the weather
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" +
+     pastSearches + "82f7826c88e27ea6666cd198fcd8453b";
 
-// // Adding click event listen listener to all buttons
-// $("button").on("click", function() {
-//     // Grabbing and storing the city to be searched property value from the button
-//     var city = $(this).attr("city-searched");
+    // Perfoming an AJAX GET request to our queryURL
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
 
-// // Constructing a queryURL using the city name
-// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-// animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+    // After the data from the AJAX request comes back
+      .then(function(response) {
 
-// var queryURL = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=82f7826c88e27ea6666cd198fcd8453b";
+//       // Saving the cities searched property
+//         var weatherData = response.
 
-// http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
+//         // Creating and storing an image tag
+//         var catImage = $("<img>");
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-//     $("#movie-view").text(JSON.stringify(response));
+//         // Setting the catImage src attribute to imageUrl
+//         catImage.attr("src", imageUrl);
+//         catImage.attr("alt", "cat image");
+
+//         // Prepending the catImage to the images div
+//         $("#images").prepend(catImage);
+//       });
 //   });
