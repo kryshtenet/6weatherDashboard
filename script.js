392f3b3020aa36a9d7aa3986f3fdb88fb33f0853
Kryshtene Tesfaye <kryshtenet@outlook.com>
2020-02-28 19:25:09 -0500

    updating java script

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
-    //Event listener for the city-search button
-    $("#citySearchBtn").on("click", function() {
-        //Grabbing and storing the data-cities property value from the button
-        var pastSearches = $(this).attr("data-cities")
+ 
+//  function searchWeatherInCities(cities){
+//    //Storing the Open Weather Map API URL for the weather
+//    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" +
+//    pastSearches + "82f7826c88e27ea6666cd198fcd8453b";
 
-    // Storing the Open Weather Map API URL for the weather
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" +
-     pastSearches + "82f7826c88e27ea6666cd198fcd8453b";
+//    //Perfoming an AJAX GET request to our queryURL
+//    $.ajax({
+//    url: queryURL,
+//    method: "GET"
+//    }).then(function(response){
 
-    // Perfoming an AJAX GET request to our queryURL
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
+//     //Printing the entire object to console
+//     console.log(response);
 
-    // After the data from the AJAX request comes back
-      .then(function(response) {
+//     // Constructing HTML containing the artist information
+//     var citiesName = $("<h1>").text(response.name);
+//     var citiesTemperature = $("<p>").attr("href", response.url).append(artistName);
+//     var citiesHumidity = $("<img>").attr("src", response.thumb_url);
+//     var citiesWindSpeed = $("<h2>").text(response.tracker_count + " fans tracking this artist");
+//     var citiesUvIndex = $("<h2>").text(response.upcoming_event_count + " upcoming events");
+//     var citiesToArtist = $("<a>").attr("href", response.url).text("See Tour Dates");
 
-//       // Saving the cities searched property
-//         var weatherData = response.
+//    })
+
+
+//  }
+ 
+ 
+ 
+ 
+ 
+ //   //Event listener for the city-search button
+  //   $("#citySearchBtn").on("click", function() {
+  //     //Grabbing and storing the data-cities property value from the button
+  //     var pastSearches = $(this).attr("data-cities")
+
+  // // Storing the Open Weather Map API URL for the weather
+  // var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" +
+  //  pastSearches + "82f7826c88e27ea6666cd198fcd8453b";
+
+  // // Perfoming an AJAX GET request to our queryURL
+  // $.ajax({
+  //   url: queryURL,
+  //   method: "GET"
+  // })
+
+  // // After the data from the AJAX request comes back
+  // .then(function(response) {
+  //   console.log(queryURL);
+  //   console.log(response);
+
+  // // Saving the cities searched property
+  // var searched = response.data
+
+
+
 
 //         // Creating and storing an image tag
 //         var catImage = $("<img>");
